918e2d32c621aafd8af4c7765443155b9e32b4f2
Alex White <alex_p_white@yahoo.co.uk>
2015-04-10 17:44:00 +0100

    investigating Interfaces

---
+++ java/src/database/Movie.java
@@ -0,0 +1,77 @@
+package database;
+
+public class Movie implements IVideo
+{
+	private String  title,
+					location,
+					genre,
+					group;
+	private int 	seriesN;
+	
+	public Movie()
+	{
+	}
+
+	@Override
+	public String getTitle()
+	{
+		return this.title;
+	}
+
+	@Override
+	public void setTitle( String title )
+	{
+		this.title = title;
+	}
+
+	@Override
+	public String getLocation()
+	{
+		return this.location;
+	}
+
+	@Override
+	public void setLocation( String location )
+	{
+		this.location = location;
+	}
+
+	@Override
+	public String getGenre()
+	{
+
+		return this.genre;
+	}
+
+	@Override
+	public void setGenre( String genre )
+	{
+		this.genre = genre;
+	}
+
+	@Override
+	public String getGroup()
+	{
+		return this.group;
+	}
+
+	@Override
+	public void setGroup( String group )
+	{
+		this.group = group;
+	}
+
+	@Override
+	public int getSeriesN()
+	{
+		return this.seriesN;
+	}
+
+	@Override
+	public void setSeriesN( int seriesN )
+	{
+		this.seriesN = seriesN;
+	}
+	
+	
+}
