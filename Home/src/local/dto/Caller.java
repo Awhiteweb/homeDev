7f1803cb3f3a0012c889b62a0643107f1e5985be
Alex White <alex_p_white@yahoo.co.uk>
2015-06-05 10:16:51 +0100

    added to caller class with filewriting and db reading options for organising databases

--- Home/src/local/dto/Caller.java
+++ Home/src/local/dto/Caller.java
@@ -1,16 +1,76 @@
 package local.dto;
 
+import java.io.FileWriter;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import local.database.mysql.MysqlConnect;
 import local.models.top.Repos;
 
 public class Caller
 {	
 	public static void main( String[] args ) throws Exception
 	{
+
+		FileWriter fw = new FileWriter( "C:\\Users\\Alex.White\\Desktop\\g-s.txt" );
 		
-		VideoProvider controller = new VideoProvider( Repos.MYSQL );
-		controller.getVideos();
+		MysqlConnect conn = new MysqlConnect();
+		String query = "SELECT * FROM `genres`";
+		ResultSet results = conn.query( query );
+		while ( results.next() )
+		{
+			int i = results.getInt( 1 );
+			String c = results.getString( 2 );
+			c = c.replace( "'", "''" );
+			String d = String.format( "UPDATE `genres` SET `genre` = '%s' WHERE `id` = %d;\n", c, i );
+			fw.write( d );
+		}
+		query = "SELECT * FROM `groups`";
+		results = conn.query( query );
+		while ( results.next() )
+		{
+			int i = results.getInt( 1 );
+			String c = results.getString( 2 );
+			c = c.replace( "'", "''" );
+			String d = String.format( "UPDATE `groups` SET `group` = '%s' WHERE `id` = %d;\n", c, i );
+			fw.write( d );
+		}
+
+		fw.close();
+		conn.close();
+
+//		VideoProvider controller = new VideoProvider( Repos.MYSQL );
+//		List<Video> videos = controller.returnVideos();
+
+//		for ( Video video : videos )
+//		{
+//			if ( video.getGroup() != null )
+//			{
+//				if ( video.getGenre().equals( "tv drama" ) )
+//				{
+//					String statement = String.format( "UPDATE `main` SET `genre` = %d WHERE `id` = %d", 25, video.getID() );
+//					controller.sendStatement( statement );
+//				}
+//			}
+//		}
+		
+//		FileWriter fw = new FileWriter( "C:\\Users\\Alex.White\\Desktop\\videos.txt" );
+//		
+//		for ( Video video : videos )
+//		{
+//			String s = video.getLocation().replace( "'", "''" );
+//			String statement = String.format( "UPDATE `main` SET `path` = '%s' WHERE `id` = %d;\n", s, video.getID() );
+//			fw.write( statement );
+//		}
+//		
+//		fw.close();
+//		System.out.println( "finished" );
+		
 		
 //		List<Video> videos = controller.returnVideos(); // gets all videos
 
